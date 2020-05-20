ed3e677b8d6d401b8b85f74c79090a7da4379ddd
Roelof Briers <rbriers@gmail.com>
2020-05-20 22:59:38 +0200

    Add next Kata

---
+++ SingletonAdamEve/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+public sealed class Adam : Male {
+
+    private Adam() : base("Adam", null, null) {
+    }
+
+    private static Adam _adam = new Adam();
+    public static Adam GetInstance ()
+    {
+        return _adam;
+    }
+}
+public sealed class Eve : Female {
+
+    private Eve() : base("Eve", null, null) {
+    } 
+    private static Eve _eve = new Eve();
+    public static Eve GetInstance (Adam adam)
+    {
+        if (adam is null)
+            throw new ArgumentNullException();
+        return _eve;
+    }
+}
+public class Male : Human {
+    private static bool first = true;
+    public Male(string name, Female mother, Male father)
+    {
+        if (!first && (mother is null || father is null))
+            throw new ArgumentNullException();
+
+        this.Father = father;
+        this.Mother = mother;
+        this.Name = name;
+        first = false;
+    }
+}
+public class Female : Human {
+
+    private static bool first = true;
+    public Female(string name, Female mother, Male father)
+    {
+        if (!first && (mother is null || father is null))
+            throw new ArgumentNullException();
+            
+        this.Father = father;
+        this.Mother = mother;
+        this.Name = name;
+        first = false;
+    }
+}
+public abstract class Human {
+    public string Name { get; set; }
+    public Human Father { get; set; }
+    public Human Mother { get; set; }
+}
